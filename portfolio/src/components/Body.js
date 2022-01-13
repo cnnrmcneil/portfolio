b6343baa47286485d5bad6ae073f5671c1190510
Connor McNeil <cnnrmcnl@gmail.com>
2022-01-13 17:31:18 -0500

    some changes

---
+++ portfolio/src/components/Body.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Body() {
+  return (
+    <div className="projects">
+      <div className="projectBox">
+        <h3>SpotList</h3>
+      </div>
+      <div className="projectBox">
+        <h3>Youtube Playlistr</h3>
+      </div>
+      <div className="projectBox">
+        <h3>Fictional Portfolio</h3>
+      </div>
+    </div>
+  );
+}
---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <h2>
+        Hi, I'm Connor McNeil. I'm a Full Stuck Developer, specializing in React
+        and JavaScript.<br></br>I was born in Scotland, and moved to the US in
+        2009. Since then{" "}
+      </h2>
+    </div>
+  );
+}
