911f29bee14413fdd94b3d8eab7d0205223fa639
Connor McNeil <cnnrmcnl@gmail.com>
2022-01-20 14:53:20 -0500

    text change

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -6,7 +6,8 @@ export default function Header() {
       <h2>
         Hi, I'm Connor McNeil. I'm a Full Stuck Developer, specializing in React
         and JavaScript.<br></br>I was born in Scotland, and moved to the US in
-        2009. Since then{" "}
+        2009. Since then, I have worked in a variety of positions and attended
+        Ironhack Miami.
       </h2>
     </div>
   );
