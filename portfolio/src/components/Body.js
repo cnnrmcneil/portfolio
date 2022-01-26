06ac9b015e3554f53765fbc8e99610f1c118496c
Connor McNeil <cnnrmcnl@gmail.com>
2022-01-26 12:41:11 -0500

    fixing project boxes

--- portfolio/src/components/Body.js
+++ portfolio/src/components/Body.js
@@ -4,17 +4,23 @@ export default function Body() {
   return (
     <div className="projects">
       <div className="projectBox">
-        <h3>SpotList</h3>
-        <p>
-          A Spotify Web App. A unique way to access your playlists in your
-          browser. Built with React.
-        </p>
+        <a href="www.sample.com">
+          <h3>SpotList</h3>
+          <p>
+            A Spotify Web App. A unique way to access your playlists in your
+            browser. Built with React.
+          </p>
+        </a>
       </div>
       <div className="projectBox">
-        <h3>Youtube Playlistr</h3>
+        <a href="www.sample.com">
+          <h3>Youtube Playlistr</h3>
+        </a>
       </div>
       <div className="projectBox">
-        <h3>Fictional Portfolio</h3>
+        <a href="www.sample.com">
+          <h3>Fictional Portfolio</h3>
+        </a>
       </div>
     </div>
   );
